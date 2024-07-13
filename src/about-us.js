82416099a583e8d7405b2fb5843d7ee897db87f4
shilpi kumari <155133954+shilpiskzz@users.noreply.github.com>
2024-07-13 08:39:31 +0530

    create sleep.js

--- src/about-us.js
+++
@@ -1,8 +0,0 @@
-// contact-us.js
-$(document).ready(function () {
-  $("form").submit(function (event) {
-    event.preventDefault();
-    // Add your form submission logic here
-    alert("Form submitted!");
-  });
-});
